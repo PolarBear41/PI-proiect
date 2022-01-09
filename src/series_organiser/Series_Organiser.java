5faaa88830266abd283219ae94d052e40d415cec
Tolbariu Andrei <80000170+PolarBear41@users.noreply.github.com>
2022-01-09 12:17:37 +0200

    Add files via upload

---
+++ src/series_organiser/Series_Organiser.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package series_organiser;
+
+/**
+ *
+ * @author Asus
+ */
+public class Series_Organiser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
